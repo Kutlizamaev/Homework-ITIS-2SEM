ef8b961868cf045aa02d437b276d645d145edbbb
Фаниль Кутлизамаев <FRKutlizamaev@stud.kpfu.ru>
2021-03-12 14:43:14 +0300

    test push

--- INTERFACE/Program.cs
+++ INTERFACE/Program.cs
@@ -9,7 +9,7 @@ namespace INTERFACE
             Number n = new Number();
             Number a = new Number();
             
-            n.num = 10;
+            n.num = 10; 
             a.num = 50;
             a.Sub(n);
         }
