91785555f5d5c94ebc02f0f0b8b392ea183b96b1
Фаниль Кутлизамаев <FRKutlizamaev@stud.kpfu.ru>
2021-03-14 18:56:16 +0300

    Закончил проект "Generics"

--- GENERICS/Program.cs
+++ GENERICS/Program.cs
@@ -6,7 +6,12 @@ namespace GENERICS
     {
         static void Main(string[] args)
         {
-            
+            Number<string> n = new Number<string>();
+            n.num = "30";
+            Number<string> n2 = new Number<string>();
+            n2.num = "20";
+            n.Sub(n2);
+            Console.WriteLine(n.num);
         }
     }
 }
